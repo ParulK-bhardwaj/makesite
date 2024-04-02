0fde07c2db27d78a256bc940e7531240419f5c23
Parul Bhardwaj <111934039+ParulK-bhardwaj@users.noreply.github.com>
2024-04-01 21:07:05 -0400

    Render the text file and print

--- makesite.go
+++ makesite.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+
 	// "io/ioutil" >> Deprecated, replaced with "os"
 	"os"
 )
+
+// Page holds all the information we need to generate a new
+// HTML page from a text file on the filesystem.
+type Page struct {
+	TextFilePath string
+	TextFileName string
+	HTMLPagePath string
+	Content      string
+}
 
 func main() {
 	fileContents, err := os.ReadFile("first-post.txt")
@@ -15,4 +26,13 @@ func main() {
 		panic(err)
 	}
 	fmt.Print(string(fileContents))
+
+	// Create a new template in memory named "template.tmpl".
+	// When the template is executed, it will parse template.tmpl,
+	// looking for {{ }} where we can inject content.
+	// .Must means that any error will result in a panic
+	tmpl := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+
+	tmpl.Execute(os.Stdout, string(fileContents))
+
 }
