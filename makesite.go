7446ad5eb06afadc140ded89850482e1d1f51fd2
Parul Bhardwaj <111934039+ParulK-bhardwaj@users.noreply.github.com>
2024-04-09 21:58:03 -0400

    Added dir flag to find all .txt files in the given directory

--- makesite.go
+++ makesite.go
@@ -4,10 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"html/template"
-	"strings"
 
 	// "io/ioutil" >> Deprecated, replaced with "os"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 // Page holds all the information we need to generate a new
@@ -20,48 +21,59 @@ func main() {
 	// flag.String: This creates a new flag of type string.
 	// It takes three arguments:
 	// the flag name, the default value, and the usage description.
-	flagPtr := flag.String("file", "first-post.txt", "The name of the file to read")
-	// dirPtr := flag.String("dir", ".", "The directory to find all .txt files")
+	dirPtr := flag.String("dir", ".", "The directory to find all .txt files")
 	flag.Parse()
 
-	fileContents, err := os.ReadFile(*flagPtr)
+	files, err := os.ReadDir(*dirPtr)
 	if err != nil {
 		// A common use of `panic` is to abort if a function returns an error
 		// value that we don’t know how to (or want to) handle. This example
 		// panics if we get an unexpected error when creating a new file.
 		panic(err)
 	}
-	fmt.Print(string(fileContents))
 
-	page := Page{
-		Content: string(fileContents),
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		if filepath.Ext(file.Name()) != ".txt" {
+			continue
+		}
+
+		fmt.Println(file.Name())
+		fileContents, err := os.ReadFile(filepath.Join(*dirPtr, file.Name()))
+		if err != nil {
+			panic(err)
+		}
+
+		// struct
+		page := Page{
+			Content: string(fileContents),
+		}
+
+		// Create a new template in memory named "template.tmpl".
+		// When the template is executed, it will parse template.tmpl,
+		// looking for {{ }} where we can inject content.
+		// .Must means that any error will result in a panic.
+		tmpl := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+
+		baseName := strings.TrimSuffix(file.Name(), ".txt")
+		newHtmlFileName := baseName + ".html"
+
+		newFile, err := os.Create(newHtmlFileName)
+		if err != nil {
+			panic(err)
+		}
+
+		// Executing the template injects the Page instance's data,
+		// allowing us to render the content of our text file.
+		// Furthermore, upon execution, the rendered template will be
+		// saved inside the new file we created earlier.
+		err = tmpl.Execute(newFile, page)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Generated HTML file: %s\n", newHtmlFileName)
 	}
-
-	// Create a new template in memory named "template.tmpl".
-	// When the template is executed, it will parse template.tmpl,
-	// looking for {{ }} where we can inject content.
-	// .Must means that any error will result in a panic.
-	tmpl := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
-
-	// Render the contents of `first-post.txt` using Go Templates and print it to stdout.
-	err = tmpl.Execute(os.Stdout, page)
-
-	if err != nil {
-		panic(err)
-	}
-
-	// output filename
-	baseName := strings.TrimSuffix(*flagPtr, ".txt")
-	newHtmlFileName := baseName + ".html"
-	// Create a new, blank HTML file.
-	newFile, err := os.Create(newHtmlFileName)
-	if err != nil {
-		panic(err)
-	}
-
-	// Executing the template injects the Page instance's data,
-	// allowing us to render the content of our text file.
-	// Furthermore, upon execution, the rendered template will be
-	// saved inside the new file we created earlier.
-	tmpl.Execute(newFile, page)
 }
